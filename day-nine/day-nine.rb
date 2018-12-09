130851f56ddedff071a76c85049b711df934cdf9
Nate Piche <nate.f.piche@gmail.com>
2018-12-09 18:57:31 -0500

    better day-nine

--- day-nine/day-nine.rb
+++ day-nine/day-nine.rb
@@ -1,52 +1,71 @@
 require 'rspec'
+
+class DoublyLinkedNode
+  attr_accessor :next, :prev
+  attr_reader :value
+
+  def initialize(v)
+    @value = v
+    @next = self
+    @prev = self
+  end
+
+  def remove
+    prev.next = @next
+    @next.prev = prev
+    value
+  end
+
+  def add(v)
+    node = DoublyLinkedNode.new(v)
+    node.prev = self
+    node.next = @next
+    @next.prev = node
+    @next = node
+  end
+
+  def rotate(k)
+    ret = self
+
+    (k - 1).times { ret = ret.prev }
+    ret
+  end
+end
+
 def part_one(player_count, final_marble)
   players = [0] * player_count
-  p_i = 3
-  i = 1
-  placed = [0, 2, 1, 3]
+  p_i = 0
+  marbles = DoublyLinkedNode.new(0)
 
-  s = Time.now
-  4.upto(final_marble) do |m|
-    if (m % 23).zero?
-      pull_i = i - 9
-
-      if pull_i < 0
-        pull_i = (pull_i + placed.size).abs
-      end
-
-      players[p_i] += (m + placed[pull_i])
-      placed.delete_at(pull_i)
-      i = pull_i + 2
+  1.upto(final_marble) do |marble|
+    if (marble % 23).zero?
+      marbles = marbles.rotate(7)
+      last = marbles.prev.remove
+      players[p_i] += marble + last
     else
-      next_insert = i > placed.size ? (i / placed.size) : i
-      placed.insert(next_insert, m)
-      i = next_insert + 2
+      marbles = marbles.next.add(marble)
     end
 
     p_i = (p_i + 1) % players.size
-    if (m % 100_000).zero?
-      e = Time.now
-      p "Marble number: [#{m}], elapsed time is #{e - s} second"
-    end
   end
 
   players.max
 end
 
 describe('part_one') do
-  xit('returns 32 when there are 9 elves and marbles go up to 25') do
+  it('returns 32 when there are 9 elves and marbles go up to 25') do
     expect(part_one(9, 25)).to eql(32)
   end
 
-  xit('returns 8317 when there are 10 elves and marbles go up to 1618') do
+  it('returns 8317 when there are 10 elves and marbles go up to 1618') do
     expect(part_one(10, 1618)).to eql(8317)
   end
 
-  xit('returns 146373 when there are 13 elves and marbles go up to 7999') do
+  it('returns 146373 when there are 13 elves and marbles go up to 7999') do
     expect(part_one(13, 7999)).to eql(146373)
   end
 
-  xit('returns 393229 when there are 441 elves and marbles go up to 71032') do
+  it('returns 393229 when there are 441 elves and marbles go up to 71032') do
     expect(part_one(441, 71032)).to eql(393229)
   end
 
